c12503c081d3817ffd7da256b5a6ec91ba274fcb
Filip Butić <filip1701@gmail.com>
2022-12-05 10:58:54 +0100

    Updated the file.

--- test_insert_row.py
+++ test_insert_row.py
@@ -1,6 +1,22 @@
+import os
 from datetime import datetime
-from app.core.models import ParkingEvent
+from core.models import ParkingEvent, ParkingEventType, ParkingSpot, ParkingSpotType, Camera
 
-par_event = ParkingEvent(license_plate="ZG123AC", time=datetime.now())
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.myapp.settings'
+# print(os.environ['DJANGO_SETTINGS_MODULE'])
 
+# cam = Camera(name=, description=)
+cam = Camera()
+cam.save()
+
+par_spot_type = ParkingSpotType(name=1)
+par_spot_type.save()
+
+par_spot = ParkingSpot(parking_spot_type=par_spot_type, camera=cam, yaml_file_location='/home/tmp/')
+par_spot.save()
+
+par_event_type = ParkingEventType(name=1)
+par_event_type.save()
+
+par_event = ParkingEvent(parking_spot=par_spot, parking_event_type=par_event_type, license_plate="ST321CA", time=datetime.now())
 par_event.save()
