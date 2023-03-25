eedb3935a908e874bf4a5c81971685aa4e9303b8
Filip Butić <filip1701@gmail.com>
2023-03-25 18:52:30 +0100

    Formatted code and moves JS to static.
    I have deleted the JS code in the index.html file and moved it to its own JS file in the static folder so it is all separate. The CSS file has been formatted.

---
+++ static/script.js
@@ -0,0 +1,46 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var drawing = false;
+var strokes = [];
+
+canvas.addEventListener('mousedown', function (e) {
+    if (!drawing) {
+        // Clear the canvas when the mouse if clicked for a new symbol to be drawn
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        strokes = [];
+    }
+    drawing = true;
+    strokes.push([e.offsetX, e.offsetY]);
+});
+
+canvas.addEventListener('mousemove', function (e) {
+    if (drawing) {
+        ctx.beginPath();
+        ctx.moveTo(strokes[strokes.length - 1][0], strokes[strokes.length - 1][1]);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        strokes.push([e.offsetX, e.offsetY]);
+    }
+});
+
+canvas.addEventListener('mouseup', function (e) {
+    drawing = false;
+    // Send the strokes data to the server for recognition
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/recognize');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            alert(xhr.responseText);
+        } else {
+            alert('Error: ' + xhr.statusText);
+        }
+    };
+    xhr.send('strokes=' + JSON.stringify(strokes));
+    strokes = [];
+});
+
+particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('loaded successfully');
+});
